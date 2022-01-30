662972651ad1e9c2106d56ada69d998cb529c17f
varun <varunsonavne@gmail.com>
2022-01-31 00:14:56 +0530

    reverted

--- demo.java
+++ demo.java
@@ -3,10 +3,14 @@ class demo{
      System.out.println("Hello Varun Jenkins Working code added here...");  
     }  
 }  
+<<<<<<< HEAD
 
 // Code added by demo... //code again added by demo
 // Yo boys
 
 // Code added by demo...//code added by test
 // Code added by test
+=======
+// Code added by demo...
+>>>>>>> parent of 20aa77e... test added
 
