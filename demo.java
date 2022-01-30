c10f153c07e644bc1d0be17279b30f9eee779cde
varun <varunsonavne@gmail.com>
2022-01-30 23:51:49 +0530

    code added

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 class demo{  
     public static void main(String args[]){  
-     System.out.println("Hello Varun Jenkins Working...");  
+     System.out.println("Hello Varun Jenkins Working code added here...");  
     }  
 }  
